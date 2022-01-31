f4406cc83e821ba33068320d7fdb7bbe610ff7e0
ashiqurras <ashiqurrussel2@gmail.com>
2022-02-01 00:54:01 +0100

    spline chart and bubble chart using apex-chart package

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                Copyright © AshiQur RusSel - 2022 Live Now. All Rights Reserved. #StaySafe
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+    return (
+        <nav>
+            <Link style={{ color: "white", textDecoration: "none" }} to="/">
+                <div style={{ fontSize: "30px" }}>🌏</div>
+            </Link>
+            <ul className="nav-links">
+                <Link
+                    style={{
+                        color: "white",
+                        textDecoration: "none",
+                        marginTop: "10px",
+                        marginRight: "40px",
+                    }}
+                    to="/symptom"
+                >
+                    <li>Symptom</li>
+                </Link>
+                <Link
+                    style={{
+                        color: "white",
+                        textDecoration: "none",
+                        marginTop: "10px",
+                        marginRight: "40px",
+                    }}
+                    to="/graph"
+                >
+                    <li>Graph</li>
+                </Link>
+                <Link
+                    style={{ color: "white", textDecoration: "none", marginTop: "10px" }}
+                    to="/about"
+                >
+                    <li>About</li>
+                </Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
